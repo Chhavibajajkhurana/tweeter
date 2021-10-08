abe48786253141c6f70f132282efb292c7d82f36
Chhavibajajkhurana <89600877+Chhavibajajkhurana@users.noreply.github.com>
2021-10-08 00:54:04 -0400

    client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,129 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(() => {
+  //Load tweets
+  loadTweets();
+
+  //Posts tweets
+  const form = $('#tweet-form');
+  form.on('submit', function(event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+    if (validateForm()) {
+      $.post('/tweets', serializedData)
+        .done(() => {
+          $('#tweet-text').val('');
+          $('.counter').val(140);
+          loadTweets();
+        })
+        .fail((error) => {
+          console.log("messed up because:", error);
+        });
+    }
+  });
+  
+  //Form visibility
+  const arrow = $('.arrow');
+  const icon = $('.icon');
+  const up = "fa-angle-double-up";
+  const down = "fa-angle-double-down";
+  arrow.on('click', function() {
+    if (form.is(":visible")) {
+      form.slideUp();
+      icon.addClass(up);
+      icon.removeClass(down);
+    } else {
+      form.slideDown();
+      icon.addClass(down);
+      icon.removeClass(up);
+      $('#tweet-text').focus();
+    }
+  });
+});
+
+const validateForm = () => {
+  const counter = Number($('.counter').val());
+  const errorElement = $('.error-notification');
+  const errorRead = $('.error-message');
+  
+  if (errorElement.is(":visible")) {
+    errorElement.slideUp();
+  }
+  if (counter === 140) {
+    let errorMessage = "Tweet field is required";
+    setTimeout(() => {
+      errorRead.text(errorMessage);
+      errorElement.slideDown('slow');
+    }, 200);
+    return false;
+  }
+  if (counter < 0) {
+    let errorMessage = "Exceeding max character count (140)";
+    setTimeout(() => {
+      errorRead.text(errorMessage);
+      errorElement.slideDown('slow');
+    }, 200);
+    return false;
+  }
+  return true;
+};
+
+const loadTweets = () => {
+  $.get('/tweets')
+    .done((response) => {
+      renderTweets(response);
+    })
+    .fail((error) => {
+      console.log("Something's wrong:", error);
+    });
+};
+
+const renderTweets = (tweets) => {
+  const $tweetsContainer = $('#tweets-container');
+  $tweetsContainer.empty();
+  for (const tweet of tweets) {
+    const $newTweet = createTweetElement(tweet);
+    $tweetsContainer.prepend($newTweet);
+  }
+};
+
+const createTweetElement = (tweet) => {
+  // tweet header
+  const $header = $('<header>');
+  const $profilePicture = $('<div class="profile-picture">');
+  const $avatar = $(`<img src=${tweet.user.avatars} alt="profile">`);
+  const $name = $('<span>').text(tweet.user.name);
+  $profilePicture.append($avatar, $name);
+  $header.append($profilePicture);
+
+  const $handle = $('<div class="handle">');
+  const $handleText = $('<p>').text(tweet.user.handle);
+  $handle.append($handleText);
+  $header.append($handle);
+
+  // tweet body
+  const $tweetContnet = $('<div class= "tweet-content">');
+  const $content = $('<p>').text(tweet.content.text);
+  const $line = $('<hr>');
+  $tweetContnet.append($content, $line);
+
+  // tweet footer
+  const $footer = $('<footer>');
+  const $date = $('<div class="date">');
+  const $day = $('<span>').text(timeago.format(tweet.created_at));
+  $date.append($day);
+  $footer.append($date);
+
+  const $reactions = $('<div class="reactions">');
+  const $flag = $('<i class="fas fa-flag"></i>');
+  const $retweet = $('<i class="fas fa-retweet"></i>');
+  const $heart = $('<i class="fas fa-heart"></i>');
+  $reactions.append($flag, $retweet, $heart);
+  $footer.append($reactions);
+
+  // tweet
+  const $tweet = $(`<article class="tweet">`);
+  $tweet.append($header, $tweetContnet, $footer);
+  return $tweet;
+};
